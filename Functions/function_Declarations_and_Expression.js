45bf7fe69be294fb203f828b8e9af41784206b9f
Murali Dharan S <89293722+murali2601@users.noreply.github.com>
2025-12-02 22:31:38 +0530

    Functions Declaration & Expression code

---
+++ Functions/function_Declarations_and_Expression.js
@@ -0,0 +1,20 @@
+
+//Functions Declaration
+
+function greet()
+{
+    console.log('Hello');
+}
+
+//Function Expression
+
+let say = function()
+{
+    console.log('Say Hello');
+} 
+
+say();
+
+let tell = say;
+
+tell();
