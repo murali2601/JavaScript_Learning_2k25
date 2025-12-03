108645163da84529dbbd77de70b5dbc0549a1591
Murali Dharan S <89293722+murali2601@users.noreply.github.com>
2025-12-03 23:22:43 +0530

    prompt user input

---
+++ prompt.js
@@ -0,0 +1,7 @@
+//getting input from user
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+let userInput = prompt("Enter the input : "); // prompt only works in browsers, not in node.js
+
+console.log(answer);
