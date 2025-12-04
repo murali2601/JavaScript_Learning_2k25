4f0e157d6e2c54abeae415056ae32396751e64c3
Murali Dharan S <89293722+murali2601@users.noreply.github.com>
2025-12-04 15:28:59 +0530

    for in loop code

---
+++ Control_Flow/for_in_loop.js
@@ -0,0 +1,15 @@
+// for in loops used to iterate over objects or arrays
+
+const person = {
+    name : 'Murali',
+    age : 24
+};
+
+for (let key in person)
+    console.log(`The ${key} is ${person[key]}`);
+
+
+const color = ['red', 'blue', 'green'];
+
+for (let index in color)
+    console.log(`The ${index} index is ${color[index]}`);
